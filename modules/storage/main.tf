6884ae3a7998698adc87a92a3add74989cd6beb7
Your Name <kulsumeumm@google.com>
2026-06-01 07:20:48 +0000

    Update POC3 verification layout with syntactically pristine HCL blocks

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,3 +1,13 @@
-variable "storage_prefix" { type = string default = "poc3-store"}
-resource "random_string" "app_store" { length = 16 special = false prefix = var.storage_prefix}
-output "storage_handle" { value = random_string.app_store.id}
+variable "storage_prefix" {
+  type    = string
+  default = "poc3-store"
+}
+
+resource "random_string" "app_store" {
+  length  = 16
+  special = false
+}
+
+output "storage_handle" {
+  value = "${var.storage_prefix}-${random_string.app_store.id}"
+}
